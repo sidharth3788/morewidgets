61a4c55e523a847ec0b628c66401e1684f6482aa
Sidharth <sidhuuosid@gmail.com>
2024-08-13 23:55:52 +0530

    .

--- lib/features/media_query.dart
+++ lib/features/media_query.dart
@@ -5,11 +5,37 @@ class MediaQueryUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // var mediaWidth = MediaQuery.of(context).
+    var mediaWidth = MediaQuery.of(context).size.width;
+    var mediaHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
       body: SafeArea(
         child: Container(
+          width: mediaWidth,
+          height: mediaHeight * 0.5,
           color: Colors.grey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Width: $mediaWidth',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text(
+                'Height: $mediaHeight',
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
